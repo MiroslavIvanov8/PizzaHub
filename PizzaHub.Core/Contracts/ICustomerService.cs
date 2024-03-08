ceda5e286c134dd9728b8564e2414a920fb33ab3
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-03-08 03:45:28 +0200

    Added customer service with some validation methods

---
+++ PizzaHub.Core/Contracts/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaHub.Core.Contracts
+{
+    public interface ICustomerService
+    {
+        Task<int> GetCustomerId(string userId);
+        Task<bool> CustomerExists(string userId);
+    }
+}
---
+++ PizzaHub.Core/Interfaces/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaHub.Core.Contracts;
+using PizzaHub.Infrastructure;
+using PizzaHub.Infrastructure.Data.Models;
+
+namespace PizzaHub.Core.Interfaces
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly PizzaHubDbContext dbContext;
+
+        public CustomerService(PizzaHubDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<int> GetCustomerId(string userId)
+        {
+            if (await CustomerExists(userId))
+            {
+                Customer? customer = await this.dbContext.Customers.FirstOrDefaultAsync(c => c.UserId == userId);
+                return customer.Id;
+            }
+
+            return 0;
+        }
+
+        public async Task<bool> CustomerExists(string userId)
+        {
+            return await this.dbContext.Customers.AnyAsync(c => c.UserId == userId);
+        }
+    }
+}
