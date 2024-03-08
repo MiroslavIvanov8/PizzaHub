4630183bf154132fedd5d6542537c9e4689ec6f5
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-03-08 03:47:25 +0200

    Created CartService with Add method

---
+++ PizzaHub.Core/Contracts/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using PizzaHub.Core.ViewModels;
+using PizzaHub.Infrastructure.Data.Models;
+
+namespace PizzaHub.Core.Contracts
+{
+    public interface ICartService
+    {
+        Task<CustomerCart> AddToCartAsync(int pizzaId, string userId, int quantity);
+
+        Task<ICollection<CartItemViewModel>> MyCartAsync(int customerId);
+    }
+}
---
+++ PizzaHub.Core/Interfaces/CartService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaHub.Core.Contracts;
+using PizzaHub.Core.ViewModels;
+using PizzaHub.Infrastructure;
+using PizzaHub.Infrastructure.Data.Models;
+
+namespace PizzaHub.Core.Interfaces
+{
+    public class CartService : ICartService
+    {
+        private readonly PizzaHubDbContext dbContext;
+        private readonly IRestaurantService restaurantService;
+        private readonly ICustomerService customerService;
+        
+
+        public CartService(PizzaHubDbContext dbContext,
+            IRestaurantService restaurantService,
+            ICustomerService customerService)
+        {
+            this.dbContext = dbContext;
+            this.restaurantService = restaurantService;
+            this.customerService = customerService;
+        }
+
+        public async Task<CustomerCart> AddToCartAsync(int itemId, string userId , int quantity)
+        {
+            if (!await this.customerService.CustomerExists(userId))
+            {
+                return null;
+            }
+
+            if (!await this.restaurantService.MenuItemExists(itemId))
+            {
+                return null;
+            }
+
+            int customerId = await this.customerService.GetCustomerId(userId);
+
+            CustomerCart cc = new CustomerCart()
+            {
+                CustomerId = customerId,
+                MenuItemId = itemId,
+                Quantity = quantity
+            };
+
+
+            await this.dbContext.CustomerCart.AddAsync(cc);
+            await dbContext.SaveChangesAsync();
+
+            return cc;
+        }
+
+        public async Task<ICollection<CartItemViewModel>> MyCartAsync(int customerId)
+        {
+            ICollection<CartItemViewModel> cartItems = await this.dbContext.CustomerCart
+                .Where(ci => ci.CustomerId == customerId)
+                .Select(i => new CartItemViewModel()
+                {
+                    Name = i.MenuItem.Name,
+                    Price = i.MenuItem.Price,
+                    Quantity = i.Quantity
+                }).ToListAsync();
+
+            return cartItems;
+        }
+    }
+}
