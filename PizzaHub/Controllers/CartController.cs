78bc3e2bfb7fab29cff5dbaffcb18d5bf4592650
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-03-08 03:48:19 +0200

    Cart controller with MyCart Implemented

--- PizzaHub/Controllers/CartController.cs
+++ PizzaHub/Controllers/CartController.cs
@@ -1,20 +1,40 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaHub.Core.Contracts;
+using PizzaHub.Core.ViewModels;
+using PizzaHub.Extensions;
 
 namespace PizzaHub.Controllers
 {
-    [Authorize]
     public class CartController : Controller
     {
-        public IActionResult Add(int itemId)
+        private readonly ICartService cartService;
+        private readonly ICustomerService customerService;
+
+        public CartController(ICartService cartService, ICustomerService customerService)
+        {
+            this.cartService = cartService;
+            this.customerService = customerService;
+        }
+
+        public async Task<IActionResult> Add(int itemId, int quantity)
         {
             if (!User.Identity.IsAuthenticated)
             {
                 return RedirectToPage("/Login");
             }
 
+            await this.cartService.AddToCartAsync(itemId, User.GetUserId(), quantity);
             
+            return RedirectToAction("Menu", "Restaurant");
+        }
+
+        public async Task<IActionResult> MyCart()
+        {
+            int customerId = await this.customerService.GetCustomerId(User.GetUserId());
+
+            ICollection<CartItemViewModel> models = await this.cartService.MyCartAsync(customerId);
+
+            return View(models);
         }
     }
 }
