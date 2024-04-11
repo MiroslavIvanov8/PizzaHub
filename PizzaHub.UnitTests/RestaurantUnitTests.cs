721d145b3bede7480ccf342116dc33d5c82c19b6
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-04-11 23:14:08 +0300

    Wrote tests for Restaurant Service

--- PizzaHub.UnitTests/RestaurantUnitTests.cs
+++ PizzaHub.UnitTests/RestaurantUnitTests.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using Microsoft.EntityFrameworkCore;
 using PizzaHub.Core.Contracts;
 using PizzaHub.Core.Services;
+using PizzaHub.Core.ViewModels.MenuItem;
 using PizzaHub.Infrastructure;
 using PizzaHub.Infrastructure.Common;
 using PizzaHub.Infrastructure.Data.Models;
@@ -106,13 +108,195 @@ namespace PizzaHub.UnitTests
         }
 
         [Test]
-        public async Task Test_Item_Exists_Returns_True()
+        public async Task Test_Item_Exists_Returns_Correct()
         {
             bool resultTrue = await this.restaurantService.MenuItemExistsAsync(1);
             bool resultFalse = await this.restaurantService.MenuItemExistsAsync(5);
 
             Assert.AreEqual(resultTrue, true);
             Assert.AreEqual(resultFalse, false);
+        }
+
+        [Test]
+        public async Task Test_GetItemReturns_Correct()
+        {
+           MenuItemViewModel margherita = await this.restaurantService.GetItemAsync(1);
+           MenuItemViewModel aNull = await this.restaurantService.GetItemAsync(10);
+
+
+           Assert.AreEqual(margherita.Id, 1);
+           Assert.AreEqual(aNull,null);
+        }
+
+        [Test]
+        [TestCase("Pizza")]
+        public async Task Test_GetMenuReturns_Correct(string searchTerm)
+        {
+            var menu = await restaurantService.GetMenuAsync(searchTerm);
+
+            Assert.IsNotNull(menu);
+            Assert.AreEqual(menu.Count(),4);
+        }
+        
+        [TestCase("Ham? Bacon!   Pineapple")]
+        public async Task Test_GetMenuReturns_Correct_If_SearchTerms_IsArr(string searchTerm)
+        {
+            var menu = await restaurantService.GetMenuAsync(searchTerm);
+
+            Assert.AreEqual(menu.Count(), 1);
+        }
+
+        [TestCase("HAM,    bacon. PIneapple, PEpper")]
+        public async Task Test_GetMenuReturns_Zero_If_SearchTerms_IsArr(string searchTerm)
+        {
+            var menu = await restaurantService.GetMenuAsync(searchTerm);
+
+            Assert.AreEqual(menu.Count(), 0);
+        }
+
+        [TestCase("pepper")]
+        public async Task Test_GetMenuReturns_Correct_If_TwoPizzas_Have_Shared_Ingredient(string searchTerm)
+        {
+            var menu = await restaurantService.GetMenuAsync(searchTerm);
+
+            Assert.AreEqual(menu.Count(), 2);
+        }
+
+        [TestCase(null)]
+        public async Task Test_GetMenuReturns_Correct_If_SearchTerm_Is_Null(string searchTerm)
+        {
+            var menu = await restaurantService.GetMenuAsync(searchTerm);
+
+            Assert.AreEqual(menu.Count(), 4);
+        }
+
+        [TestCase("chips")]
+        public async Task Test_GetMenuReturns_Correct_If_SearchTerm_Is_Not_Available(string searchTerm)
+        {
+            var menu = await restaurantService.GetMenuAsync(searchTerm);
+
+            Assert.AreEqual(menu.Count(), 0);
+        }
+
+        [TestCase("    asda sdda sda sdasdwasdwasd ")]
+        public async Task Test_GetMenuReturns_Correct_If_SearchTerm_Is_EmptyString_Or_NonSense(string searchTerm)
+        {
+            var menu = await restaurantService.GetMenuAsync(searchTerm);
+
+            Assert.AreEqual(menu.Count(), 0);
+        }
+
+        [Test]
+        public async Task Test_ShowBestSellers_Should_Return_Correct_If_No_Data()
+        {
+            var bestSellers = await restaurantService.ShowBestSellersAsync();
+
+            Assert.AreEqual(bestSellers.Count(), 0);
+        }
+
+        [Test]
+        public async Task Test_ShowBestSellers_Should_Return_Correct_With_Data()
+        {
+            OrderItem orderItem1 = new OrderItem()
+            {
+                Id = 1,
+                MenuItemId = 1,
+                Quantity = 3
+            };
+            OrderItem orderItem2 = new OrderItem()
+            {
+                Id = 2,
+                MenuItemId = 2,
+                Quantity = 2
+            };
+            OrderItem orderItem3 = new OrderItem()
+            {
+                Id = 3,
+                MenuItemId =3,
+                Quantity = 1
+            };
+
+            await repository.AddAsync(orderItem1);
+            await repository.AddAsync(orderItem2);
+            await repository.AddAsync(orderItem3);
+            await repository.SaveChangesAsync();
+
+            var bestSellers = await restaurantService.ShowBestSellersAsync();
+
+            Assert.AreEqual(bestSellers.Count(), 3);
+        }
+
+        [Test]
+        public async Task Test_ShowBestSellers_Should_Return_Correct_With_Only_Two()
+        {
+            OrderItem orderItem1 = new OrderItem()
+            {
+                Id = 1,
+                MenuItemId = 1,
+                Quantity = 3
+            };
+            OrderItem orderItem2 = new OrderItem()
+            {
+                Id = 2,
+                MenuItemId = 2,
+                Quantity = 2
+            };
+            
+
+            await repository.AddAsync(orderItem1);
+            await repository.AddAsync(orderItem2);
+            await repository.SaveChangesAsync();
+
+            var bestSellers = await restaurantService.ShowBestSellersAsync();
+
+            Assert.AreEqual(bestSellers.Count(), 2);
+        }
+
+        [Test]
+        public async Task Test_ShowBestSellers_Should_Return_Correct_Quantity_Items()
+        {
+            OrderItem orderItem1 = new OrderItem()
+            {
+                Id = 1,
+                MenuItemId = 1,
+                Quantity = 4
+            };
+            OrderItem orderItem2 = new OrderItem()
+            {
+                Id = 2,
+                MenuItemId = 2,
+                Quantity = 3
+            };
+            OrderItem orderItem3 = new OrderItem()
+            {
+                Id = 3,
+                MenuItemId = 3,
+                Quantity = 2
+            };
+
+            OrderItem orderItem4 = new OrderItem()
+            {
+                Id = 4,
+                MenuItemId = 4,
+                Quantity = 1
+            };
+            await repository.AddAsync(orderItem1);
+            await repository.AddAsync(orderItem2);
+            await repository.AddAsync(orderItem3);
+            await repository.AddAsync(orderItem4);
+
+            await repository.SaveChangesAsync();
+
+            var bestSellers = await restaurantService.ShowBestSellersAsync();
+
+            List<int> expectedItemIds = new List<int> { 1, 2, 3 };
+            
+            Assert.AreEqual(bestSellers.Count(), 3);
+
+            foreach (var bestSeller in bestSellers)
+            {
+                Assert.IsTrue(expectedItemIds.Contains(bestSeller.Id));
+            }
         }
     }
 }
